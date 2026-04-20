ff8c0148d18223e77f5145a79c287e2dadde4741
Raghav Bhardwaj <bhardwajraghav672@gmail.com>
2026-04-20 10:21:26 +0530

    fixed server

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,26 @@
-require("dotenv").config({path:'./backend/.env'});
+require("dotenv").config({path: "./backend/.env"});
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
 
+
 app.use(cors());
 app.use(express.json());
 
+//console.log(process.env.MONGO_URI);
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
 app.use("/api/auth", require("./routes/auth"));
+
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(process.env.PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`)
